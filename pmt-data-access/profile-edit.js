36bfcce182e42f28fecf3f8fb6e5c027ef1b32bf
azahnen <zahnen@interactive-instruments.de>
2017-10-22 11:32:27 +0200

    fix consistency check on type removal

--- pmt-data-access/profile-edit.js
+++ pmt-data-access/profile-edit.js
@@ -297,7 +297,8 @@ function getProfileUpdatesForType(id, modelId, profile, include) {
 
     if (!include) {
         return modelReader.getAllOfType(id, modelId, {}, {
-            optional: true
+            // return mandatory as well for consistency checks
+            //optional: true
         })
             .then(function(classesOfType) {
                 return classesOfType.filter(function(cls) {
@@ -319,6 +320,17 @@ function getProfileUpdatesForType(id, modelId, profile, include) {
                             buildPropertyUpdate(update, profile, include, index)
                         }
                     })
+
+                    // if type was removed, return classes with mandatory properties of type
+                    // so that consistency can be validated
+                    if (Object.keys(update).length === 0) {
+                        cls.properties.forEach(function(prp, index) {
+                            if (!include && !prp.optional && prp.typeId === id) {
+                                //update['$pull']['properties.' + i + '.profiles'] = profile;
+                                buildPropertyUpdate(update, profile, !include, index)
+                            }
+                        })
+                    }
 
                     return profileWriter.putClassUpdateProfile(cls.localId, modelId, update)
                 })
